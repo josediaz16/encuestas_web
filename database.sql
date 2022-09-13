25e10dba5fc6869b69aa588da3e211dbcc4821f8
josediaz16 <jldiaz1612@gmail.com>
2022-09-12 20:01:35 -0500

    Crear base de datos

---
+++ database.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS encuestas_web;
+CREATE DATABASE encuestas_web;
+
+\c encuestas_web;
+
+CREATE TABLE paises (
+  id serial PRIMARY KEY,
+  nombre VARCHAR ( 255 ) UNIQUE NOT NULL
+);
+
+CREATE TABLE encuestas (
+  id serial PRIMARY KEY,
+  nombre VARCHAR ( 255 ) NOT NULL,
+  sexo VARCHAR ( 1 ) NOT NULL,
+  edad INT NOT NULL,
+  id_pais INT NOT NULL,
+  FOREIGN KEY (id_pais) REFERENCES paises (id)
+);

fce69b6bc86c7f8258d30f16eebd584579bb6425
josediaz16 <jldiaz1612@gmail.com>
2022-09-14 21:01:17 -0500

    agregar edicion y borrado de encuestas

--- app.rb
+++ app.rb
@@ -15,7 +15,14 @@ end
 get '/encuesta' do
   paises = conn.exec("SELECT * FROM paises").to_a
 
-  erb :encuesta, locals: { paises: paises }
+  erb :encuesta, locals: { paises: paises, encuesta: {}, accion: '/enviar_encuesta', verbo: 'POST' }
+end
+
+get '/encuestas/:id' do
+  encuesta = conn.exec("SELECT * FROM encuestas WHERE id = #{params[:id]}").to_a.first # { id: 1, nombre: ... }
+  paises = conn.exec("SELECT * FROM paises").to_a
+
+  erb :encuesta, locals: { paises: paises, encuesta: encuesta, accion: "/encuestas/#{params[:id]}", verbo: 'POST' }
 end
 
 post '/enviar_encuesta' do
@@ -29,7 +36,38 @@ post '/enviar_encuesta' do
   redirect '/encuestas'
 end
 
+post '/encuestas/:id' do
+  sql = """
+    UPDATE encuestas
+    SET
+      nombre = '#{params[:nombre]}',
+      sexo =  '#{params[:sexo]}',
+      edad = #{params[:edad]},
+      id_pais = #{params[:id_pais]}
+    WHERE id = #{params[:id]}
+  """
+  conn.exec(sql)
+
+  redirect '/encuestas'
+end
+
+post '/encuestas/borrar/:id' do
+  sql = "DELETE FROM encuestas WHERE id = #{params[:id]}"
+  conn.exec(sql)
+
+  redirect '/encuestas'
+end
+
+delete '/encuestas/borrar_e/:id' do
+  sql = "DELETE FROM encuestas WHERE id = #{params[:id]}"
+  conn.exec(sql)
+
+  redirect '/encuestas'
+end
+
 get '/encuestas' do
   encuestas = conn.exec("SELECT encuestas.*, paises.nombre AS nombre_pais FROM encuestas INNER JOIN paises ON paises.id= encuestas.id_pais").to_a
   erb :encuestas, locals: { encuestas: encuestas }
 end
+
+# CRUD
